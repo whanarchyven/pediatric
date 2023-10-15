25e3900e29eda2b84c6b411315a98c40de23142c
nickerlan <nickerlandev@gmail.com>
2023-10-15 22:17:02 +0300

    fix admin

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -281,6 +281,97 @@ export declare const app: Elysia<"", {
             };
         };
     };
+    "/user/:user_uuid/participations/byEventId/:event_id": {
+        get: {
+            body: unknown;
+            params: unknown;
+            query: unknown;
+            headers: unknown;
+            response: {
+                200: Promise<{
+                    registration: (mongoose.Document<unknown, {}, {
+                        email: string;
+                        invoice_id: number;
+                        out_sum: number;
+                        status?: string | undefined;
+                        meta?: {
+                            query?: string | undefined;
+                            participationType?: string | undefined;
+                            phone?: string | undefined;
+                        } | undefined;
+                        _id?: mongoose.Types.ObjectId | undefined;
+                        customer?: string | undefined;
+                        event?: string | undefined;
+                        transactions?: {
+                            initial?: {
+                                amountRub: number;
+                            } | undefined;
+                        } | undefined;
+                        after_reg_email_subject?: string | undefined;
+                        after_reg_email_body?: string | undefined;
+                        after_reg_email_placeholders?: {
+                            name?: string | undefined;
+                            participationType?: string | undefined;
+                            event_name?: string | undefined;
+                            event_id?: string | undefined;
+                        } | undefined;
+                        after_pay_email_subject?: string | undefined;
+                        after_pay_email_body?: string | undefined;
+                        after_pay_email_placeholders?: {
+                            name?: string | undefined;
+                            participationType?: string | undefined;
+                            event_name?: string | undefined;
+                            event_id?: string | undefined;
+                        } | undefined;
+                        user_email?: string | undefined;
+                        register_timestamp?: Date | undefined;
+                        after_pay_email_text?: string | undefined;
+                        cert?: string | undefined;
+                    }> & {
+                        email: string;
+                        invoice_id: number;
+                        out_sum: number;
+                        status?: string | undefined;
+                        meta?: {
+                            query?: string | undefined;
+                            participationType?: string | undefined;
+                            phone?: string | undefined;
+                        } | undefined;
+                        _id?: mongoose.Types.ObjectId | undefined;
+                        customer?: string | undefined;
+                        event?: string | undefined;
+                        transactions?: {
+                            initial?: {
+                                amountRub: number;
+                            } | undefined;
+                        } | undefined;
+                        after_reg_email_subject?: string | undefined;
+                        after_reg_email_body?: string | undefined;
+                        after_reg_email_placeholders?: {
+                            name?: string | undefined;
+                            participationType?: string | undefined;
+                            event_name?: string | undefined;
+                            event_id?: string | undefined;
+                        } | undefined;
+                        after_pay_email_subject?: string | undefined;
+                        after_pay_email_body?: string | undefined;
+                        after_pay_email_placeholders?: {
+                            name?: string | undefined;
+                            participationType?: string | undefined;
+                            event_name?: string | undefined;
+                            event_id?: string | undefined;
+                        } | undefined;
+                        user_email?: string | undefined;
+                        register_timestamp?: Date | undefined;
+                        after_pay_email_text?: string | undefined;
+                        cert?: string | undefined;
+                    } & Required<{
+                        _id: mongoose.Types.ObjectId;
+                    }>) | null;
+                }>;
+            };
+        };
+    };
     "/user/:user_uuid/participations/byEventId/:event_id/getCert": {
         get: {
             body: unknown;
