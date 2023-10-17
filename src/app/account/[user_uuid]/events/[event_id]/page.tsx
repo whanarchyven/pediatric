b597ec465b2f2a1b7f98f2a5215ee1664f29deee
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-10-17 17:30:19 +0300

    added ticket page

--- src/app/account/[user_uuid]/events/[event_id]/page.tsx
+++ src/app/account/[user_uuid]/events/[event_id]/page.tsx
@@ -26,129 +26,71 @@ import {classList} from "@/helpers/classList";
 // import required modules
 
 
-export default function Home(params: { params: { user_uuid: string } }) {
+export default function Home(params: { params: { user_uuid: string,event_id:string } }) {
 
     const user_uuidTemp = params.params.user_uuid
-    const images = '/pages/account'
-    // @ts-ignore
-    const publicationsData = useEden(() => eden.publication.list.get())
+    const event_id=params.params.event_id
 
-    const {publications}=publicationsData?.data?? {} as any
+    console.log(user_uuidTemp);
+    console.log(event_id);
 
+    const [event,setEvent]=useState<any>()
 
-    const {data} = useEden(() => eden.user[user_uuidTemp].profile.get())
+    const {data}=useEden(()=>eden.user[user_uuidTemp].participation[event_id].get())
+    console.log(data)
 
-    const {
-        uuid,
-    } = data?.profile ?? {} as any;
+    const [ticketLink,setTicketLink]=useState('/')
 
+    useEffect(() => {
+        eden.event.byId[event_id].get().then((res)=>{
+            setEvent(res.data.events)
+        })
+    }, []);
 
-    const participationsTemp=useEden(()=>eden.user[user_uuidTemp].participations.get())
-    const participations=participationsTemp?.data?? {} as any
-
-
-    const eventsTempData=useEden(()=>eden.event.list.get())
-    const events=eventsTempData?.data?.events
-
-    const certLink = (event_id:string)=>
-    `/api2/user/my/participations/byEventId/${event_id}/getCert`
-  
-
-
-    // console.log(participations)
-
-    // console.log(events)
-
-    const [isQrCodeOpen,setIsQrCodeOpen]=useState(false)
-
-    const [activeQr,setActiveQr]=useState('')
+    useEffect(() => {
+        eden.user[user_uuidTemp].participation[event_id].getTicketLink.get().then((res)=>{
+            if(res?.data?.ticketLink){
+                setTicketLink(res?.data?.ticketLink)
+            }
+        })
+    }, []);
 
     const router=useRouter();
 
     return (
         <main className={'p-2 lg:p-12'}>
             <div className={'flex justify-between'}>
-                <p className={'uppercase font-inter font-extralight text-2xl lg:text-3xl'}>Мероприятия, на которые <br/><span
-                    className={'font-extrabold'}>вы зарегистрировались</span></p>
+                <p className={'uppercase font-inter font-extralight text-2xl lg:text-3xl'}>Ваш Билет <br/><span
+                    className={'font-extrabold'}>на мероприятие</span></p>
             </div>
-            {isQrCodeOpen?<QrPop closeFunc={()=>{setIsQrCodeOpen(false)}} activeQrLink={activeQr}></QrPop>:null}
-
-            {participations && participations.length > 0 ? <div className={'flex mt-16 border-t-[1px] border-green flex-col'}>
-                <div className={'lg:grid hidden border-b-[1px] border-green grid-cols-12'}>
-                    <div className={'col-span-1 text-white font-bold bg-green text-sm flex items-center justify-start border-l-[1px] p-3 border-green'}>
-                        Дата
+            <p className={'my-12 text-2xl font-bold'}>{event?.name} <br/> <span className={'text-green'}>{event?.date}</span></p>
+            <div className={'grid mt-10 grid-cols-2 gap-12'}>
+                <div className={'aspect-video flex items-center relative'}>
+                    <img className={'w-full h-full object-cover rounded-lg'} src={event?.layoutBg}/>
+                    <img className={'w-1/2 aspect-square absolute left-5'} src={data?.qrCodeUrl}/>
+                </div>
+                <div className={'flex flex-col gap-2'}>
+                    <div className={'grid grid-cols-3'}>
+                        <p className={'font-bold text-xl'}>ФИО:</p>
+                        <p className={'font-bold col-span-2 text-green text-xl'}>{data?.info?.name}</p>
                     </div>
-                    <div className={'col-span-6 text-white font-bold bg-green text-sm  border-l-[1px] p-3 border-white'}>
-                        Название мероприятия
+                    <div className={'grid grid-cols-3'}>
+                        <p className={'font-bold text-xl'}>Тип участия:</p>
+                        <p className={'font-bold col-span-2 text-green text-xl'}>{data?.info?.participationType}</p>
                     </div>
-                    <div className={'col-span-1 text-white font-bold bg-green text-sm lg:border-r-0 border-r-[1px] border-l-[1px] p-3 border-white'}>
-                        Оплачено
+                    <div className={'grid grid-cols-3'}>
+                        <p className={'font-bold text-xl'}>Оплачено: </p>
+                        <p className={'font-bold col-span-2 text-green text-xl'}>{data?.sum} ₽</p>
                     </div>
-                    <div className={'col-span-2 text-white font-bold bg-green text-sm lg:border-r-0 border-r-[1px] border-l-[1px] p-3 border-white'}>
-                        Формат участия
-                    </div>
-                    <div className={'col-span-2 text-white font-bold bg-green text-sm lg:border-r-[1px] border-l-[1px] border-r-[1px] p-3 border-r-green border-l-white'}>
-                        Билет
+                    <div className={'flex flex-col gap-5 mt-6'}>
+                        <p className={'text-xl font-light'}>Предъявите данный QR-код на входе в место проведения мероприятия</p>
+                        <p className={'text-xl font-light'}>Также вы можете <span className={'font-bold'}>скачать</span> свой билет в Google Wallet или Apple Wallet</p>
+                        <Link href={ticketLink} className={'w-96 bg-green p-3 flex items-center justify-center rounded-lg font-bold text-white'}>
+                            Скачать билет
+                        </Link>
                     </div>
                 </div>
-                {participations?.map((participation:typeof participations[0],counter:number)=>{
-                    let nowDate = new Date();
-                    nowDate.setHours(0, 0, 0, 0)
-                    let lexems = events?.find(item=>item.id==participation.info.event_id)?.date.split('.')
-                    let isTranslationAvailable=false
-                    console.log(lexems);
-                    if(lexems[0].length>2){
-                        let sublexems=lexems[0].split('-')
-                        if (events&&new Date(`${lexems[1]}/${sublexems[0]}/${lexems[2]}`) <= nowDate) {
-                            isTranslationAvailable=true
-                        }
-                    }else{
-                        if (events&&new Date(`${lexems[1]}/${lexems[0]}/${lexems[2]}`) <= nowDate) {
-                            isTranslationAvailable=true
-                        }
-                    }
-                    return(
-                        // <PublicationTab user_uuid={uuid} {...participation} key={participation.title}></PublicationTab>
-                        <div key={counter} className={'grid  border-b-[1px] border-green  lg:grid-cols-12'}>
-                            <div className={'lg:col-span-1 flex items-center text-sm justify-start lg:border-r-0 border-r-[1px] border-l-[1px] p-3 border-green'}>
-                                {events?.find(item=>item.id==participation.info.event_id)?.date}
-                            </div>
-                            <div className={'lg:col-span-6 lg:border-r-0 text-sm border-r-[1px] border-l-[1px] p-3 border-green'}>
-                                {participation.info.event_name}
-                            </div>
-                            <div className={'lg:col-span-1 lg:border-r-0 text-sm border-r-[1px] border-l-[1px] p-3 border-green'}>
-                                {participation.sum}
-                            </div>
-                            <div className={'lg:col-span-2 lg:border-r-0 text-sm border-r-[1px] border-l-[1px] p-3 border-green'}>
-                                {participation.info.participationType}
-                            </div>
-                            <div className={'lg:col-span-2 border-r-[1px] text-sm  border-l-[1px] border-r-[1px] p-3 border-green'}>
-                                {participation.info.participationType=='онлайн'?<div onClick={()=>{
-                                    if(isTranslationAvailable) {
-                                        router.push(`/events/${participation.info.event_id}`)
-                                    }
-                                }} className={classList('p-4 flex rounded-lg cursor-pointer items-center justify-center font-bold text-white',isTranslationAvailable?'text-sm bg-green':'bg-zinc-700 text-xs')}>{isTranslationAvailable?'Перейти к трансляции':'Трансляция будет доступна в день мероприятия'}</div>:<div onClick={()=>{
-                                    setActiveQr(participation.qrCodeUrl)
-                                    setIsQrCodeOpen(true)
-                                }} className={'p-4 bg-green flex rounded-lg cursor-pointer items-center justify-center font-bold text-white'}>
-                                    Скачать билет
-                                </div>}
-                                {participation.cert&&<a 
-                                href={certLink(participation.info.event_id)}
-                                target={"blank"}
-                                rel="noreferer"
-                                className={'mt-2 p-4 bg-green flex rounded-lg cursor-pointer items-center justify-center font-bold text-white text-sm'}>
-                                    Скачать сертификат
-                                </a>}
-                            </div>
-                        </div>
-                    )
-                })}
-            </div> : <div
-                className={'flex h-52 border-[1px] border-green rounded-lg items-center justify-center'}>
-                <p className={'opacity-50'}>Мероприятия не найдены</p>
-            </div>}
-
+            </div>
 
         </main>
     )
