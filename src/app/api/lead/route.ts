724a715f71945d49bfa92b94503a6c2e1fd22fe2
Ivan Zaburdaev <ivanlom227@gmail.com>
2025-03-16 07:47:54 +0300

    robokassa fixes

--- src/app/api/lead/route.ts
+++ src/app/api/lead/route.ts
@@ -4,13 +4,17 @@ import axios from "axios";
 export async function POST(request: Request) {
     try {
         const data = await request.json();
-        console.log(data);
-        const result=await axios.post('https://ars.ptq.pw/mg/pderm/crm/getLink', data,{headers:{
+        console.log(JSON.stringify(data),"INitial data");
+      
+        const result=await axios.post('https://ars.ptq.pw/mg/pderm/crm/getLink', {...data,after_reg_email_subject:"Благодарим за регистрацию"},{headers:{
             Authorization:'Bearer dBNSFaf9hwHEx7ZLRS9mFaUM7MvD4U2ZVfdHZJ8qgKELpQ8MJL'
             }})
-
+            console.log(result.data,"RESULT AUE");
+       
         return new NextResponse(JSON.stringify(result.data));
     } catch (error: any) {
+        console.log(error,"ERROR AUE");
+        
         return new NextResponse(JSON.stringify(error));
     }
 }
