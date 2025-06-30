71c624fd771b31c1911c932fd60ac6801b05f287
Ivan Zaburdaev <ivanlom227@gmail.com>
2025-06-30 14:32:24 +0300

    open stream

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -807,7 +807,7 @@ export default function Page({params}: any) {
                 </div> : null}
 
 
-            {haveAccessToStream||event?.date=='07.06.2025' ?
+            {haveAccessToStream ?
                 <div className={'bg-white relative lg:py-0 py-12 px-[20px] lg:px-[90px] '}>
                     <div id={'form'} className={'absolute -top-40'}></div>
                     <div
